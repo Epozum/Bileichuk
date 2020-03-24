fe97d5fee911cd856f4ab49b118b116ec35097d3
Epozum <56679515+Epozum@users.noreply.github.com>
2020-03-24 15:17:47 +0200

    Add files via upload

---
+++ practichna9/pr9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pr9
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(7);
+            Fraction b = new Fraction(5);
+            Console.WriteLine($"a = {a.get_digit()}");
+            Console.WriteLine($"b = {b.get_digit()}");
+
+            Console.WriteLine($"a + b = {a + b}");
+
+            Console.WriteLine($"a - b = {a - b}");
+
+            Console.WriteLine($"a * b = {a * b}");
+
+            Console.WriteLine($"a / b = {a / b}");
+
+            Console.WriteLine($"a > b = {a > b}");
+
+            Console.WriteLine($"a >= b = {a >= b}");
+
+            Console.WriteLine($"a < b = {a < b}");
+
+            Console.WriteLine($"a <= b = {a <= b}");
+
+            Console.WriteLine($"a == b = {a == b}");
+
+            Console.WriteLine($"a != b = {a != b}");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
